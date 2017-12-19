628eda29a0957a830fbdb6e4b812f89b04620328
wlddxxmmsg <32234982+wlddxxmmsg@users.noreply.github.com>
2017-12-19 20:18:22 +0800

    Add files via upload

--- 第二小数.cpp
+++ 第二小数.cpp
@@ -1,29 +1,29 @@
 #include<stdio.h>
 int main(){
-	int i,j,d,n,m,a[100];
+	int i,j,d,n,m,a[100],b[100];
 	scanf("%d\n", &n);
 	for(i=1;i<=n;i++){
 		scanf("%d",&m);
-		if(m=1)
-		printf("ERROR\n");
-		else{
-			for(j=1;j<=m;j++)
-			scanf("%d",&a[j]);
+		for(j=1;j<=m;j++)
+		scanf("%d",&a[j]);
+		for(int i = 1 ; i <= m-1 ; i++){
 			int c=1;
-			for(j=2;j<=m-1;j++)
-			if(a[j]<a[c])
+			for(j=2;j<=m-i+1;j++)
+			if(a[j]>a[c])
 			c=j;
-			d=a[m];
-			a[m]=a[c];
+			d=a[m-i+1];
+			a[m-i+1]=a[c];
 			a[c]=d;
-			int e=1;
-			for(j=2;j<=m-2;j++)
-			if(a[j]<a[e])
-			e=j;
-			if(a[e]=a[m])
-			printf("ERROR\n");
-			else printf("%d\n",a[e]);
-		}	
+		}
+	int e=1;
+	b[e++]=a[1];
+	for(j=2;j<=m;j++)
+	if(a[j]!=a[j-1])
+	b[e++] = a[j];
+	if(e<2)
+	printf("ERROR\n");
+	else
+	printf("%d\n",b[2]);		
 	}
 	return 0;
 } 
