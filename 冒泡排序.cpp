47be3dab1083e8b53b4ca7a1ee70e4dd963101b8
wlddxxmmsg <32234982+wlddxxmmsg@users.noreply.github.com>
2017-12-26 20:30:35 +0800

    修改

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -5,7 +5,7 @@ int main(){
 	for(int i=1;i<=n;i++)
 	scanf("%d",&a[i]);
 	for(int i=1;i<n;i++){
-		for(int j=1;j<=n-i+1;j++){
+		for(int j=1;j<n-i+1;j++){
 		    if(a[j]>a[j+1]){
 			  int t=a[j+1];
 			  a[j+1]=a[j];
@@ -13,7 +13,7 @@ int main(){
 		    }
 	    } 
 		for(int j=1;j<n;j++)
-		printf("%d",a[j]);
+		printf("%d ",a[j]);
 	    printf("%d",a[n]);
 	    printf("\n");
 	}
